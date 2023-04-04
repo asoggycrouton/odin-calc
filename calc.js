d371aa0106e3f743bd586ad8256614d28c06f07a
Antyon <antyon@me.com>
2023-04-04 16:02:42 -0400

    Operator functions

--- calc.js
+++ calc.js
@@ -33,6 +33,10 @@ document.addEventListener('DOMContentLoaded', function(){
         currentScreen.textContent = currentValue;
     })
 
+    equal.addEventListener('click', function() {
+        calculate()
+    })
+
 });
 
 function handleNumber(num) {
@@ -47,3 +51,19 @@ function handleOperator(op) {
     previousValue = currentValue;
     currentValue = '';
 }
+
+function calculate() {
+    previousValue = Number(previousValue);
+    currentValue = Number(currentValue);
+    
+    if(operator === '+') {
+        previousValue += currentValue;
+    } else if(operator === '-') {
+        previousValue -= currentValue;
+    } else if(operator === 'x') {
+        previousValue *= currentValue;
+    } else{
+        previousValue /= currentValue;
+    }
+    console.log(previousValue);
+};
